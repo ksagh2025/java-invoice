9ed56a4f676dc9ce9d0faa8d2de2f6c068206e02
Krzysztof Sieja <ksieja@student@agh.edu.pl>
2026-01-11 15:58:51 +0100

    komentarze - altermatywne rozwiazanie - mapa

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -8,10 +8,13 @@ import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
     private Collection<Product> products = new ArrayList<>();
+    //alternatywnie można uzyć mapy do trzymani ailości produktów;
 
     public void addProduct(Product product) {
         if (product == null) throw new IllegalArgumentException("product cannot be null");
         this.products.add(product);
+        //alternatywnie - wykorzystujemy metodę dla wielu produktów >>> this.addProduct(product, 1)
+
     }
 
     public void addProduct(Product product, Integer quantity) {
@@ -19,6 +22,7 @@ public class Invoice {
         if (quantity <= 0) throw new IllegalArgumentException("Quantity cannot be zero");
         for (int i = 0; i < quantity; i++) {
         products.add(product);}
+        //alternatywnie : this.products.put(product,quantity);
     }
 
     public BigDecimal getSubtotal() {
@@ -27,6 +31,9 @@ public class Invoice {
             total = total.add(product.getPrice());
         }
         return total;
+        //alternatywnie >>>> wykorzysta mapy
+        //for (Product product : this.products.keyset()){
+        // Integer quantity }
     }
 
     public BigDecimal getTax() {
@@ -35,6 +42,8 @@ public class Invoice {
             total = total.add(product.getPriceWithTax().subtract(product.getPrice()));
         }
         return total;
+
+
     }
 
     public BigDecimal getTotal() {
